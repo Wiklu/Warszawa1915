c88320ee4abcc1d66d3901c7c4026a1407aa1b75
AleksandraKaminska <kaminska97@live.com>
2017-07-31 02:13:25 +0200

    initial commit

--- js/index.js
+++ js/index.js
@@ -14,3 +14,11 @@ var mySwiper = new Swiper(".swiper-container", {
 });
 
 $('.map').scrollTop(1200).scrollLeft(1100);
+$('.plus').on('click', function() {
+  $('.map').find('img').css('width', '+=200');
+  $('.map').find('img').css('height', '+=200');
+});
+$('.minus').on('click', function() {
+  $('.map').find('img').css('width', '-=200');
+  $('.map').find('img').css('height', '-=200');
+});
