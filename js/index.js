1115526857409dc325338c758adc7393fb31d11b
AleksandraKaminska <kaminska97@live.com>
2017-08-05 16:06:27 +0200

    initial commit

--- js/index.js
+++ js/index.js
@@ -1,18 +1,3 @@
-var mySwiper = new Swiper(".swiper-container", {
-  direction: "vertical",
-  loop: false,
-  pagination: ".swiper-pagination",
-  grabCursor: true,
-  speed: 500,
-  keyboardControl: true,
-  paginationClickable: true,
-  parallax: true,
-  effect: "slide",
-  mousewheelControl: 1,
-  hashnav: true,
-  hashnavWatchState: true
-});
-
 $('.map').scrollTop(1200).scrollLeft(1100);
 $('.plus').on('click', function() {
   $('.map').find('img').css('width', '+=200');
