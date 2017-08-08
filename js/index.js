87cdd35627c2e978cc72a2ab238abeb04cf698fa
AleksandraKaminska <kaminska97@live.com>
2017-08-08 22:46:37 +0200

    initial commit

--- js/index.js
+++ js/index.js
@@ -1,7 +1,30 @@
 $('map').imageMapResize();
 
+$(window).scroll(function() {
+    var height = $(window).scrollTop();
+    // if(height >= 1100) {
+    //     console.log(height);
+    // }
+});
+
 $('.map').scrollTop(1200).scrollLeft(1100);
 var map = $('.map').find('img');
+
+// $('.paragraph').each(function() {
+//   if ($(this)[0].offsetHeight * 1.5 < $(this)[0].scrollHeight) {
+//     console.log($(this)[0].offsetHeight);
+//     console.log('scroll: ',$(this)[0].scrollHeight);
+//     $(this).parent().width('100%').height('50%').css({'left': '0', "z-index": '0'});
+//   }
+// });
+
+// $('.paragraph').on('click', function() {
+//   if ($(this)[0].offsetHeight !== $(this)[0].scrollHeight) {
+//     console.log($(this).parent().width('43%').height('50%'));
+//   }
+//   console.log($(this)[0].offsetHeight);
+//   console.log('scroll: ',$(this)[0].scrollHeight);
+// });
 
 $('.plus').on('click', function() {
   var newWidth = map.width() * 1.1;
